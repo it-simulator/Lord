c5a49ece63b27c8d4846db07400f21e1ebdee6a9
Daria <106304366+Loza-ya@users.noreply.github.com>
2022-06-28 23:48:41 +0700

    Update say hi_v2.py.py

--- say hi_v2.py.py
+++ say hi_v2.py.py
@@ -5,6 +5,7 @@ from telebot import types
 
 update_id = None
 
+#из задачи со встречи
 #while True:
  # contents = httpx.get('https://api.telegram.org/bot5479838908:AAGACo54yOGqeY2CgiyfPbu_pUpX04pH98g/getUpdates')
  # updates = contents.json()
@@ -14,13 +15,16 @@ update_id = None
   #if new_update_id != update_id:
   #    update_id = new_update_id
 
+#если тип сообщения = команда "старт", Бот отвечает: Hi, Bro!  
 commands=['start']
 httpx.get('https://api.telegram.org/bot5479838908:AAGACo54yOGqeY2CgiyfPbu_pUpX04pH98g/sendMessage?chat_id=5301913989&text=Hi, Bro!')
 
+#если тип сообщения = текст и текст = "hi", Бот отвечает: How are you?
 content_types=['text']
 if message.text == 'hi':
  httpx.get('https://api.telegram.org/bot5479838908:AAGACo54yOGqeY2CgiyfPbu_pUpX04pH98g/sendMessage?chat_id=5301913989&text=How are you?')
-     
+ 
+#если тип сообщения = текст и текст = "by",Бот отвечает: By, Bro! 
 elif message.text == "by":
  httpx.get('https://api.telegram.org/bot5479838908:AAGACo54yOGqeY2CgiyfPbu_pUpX04pH98g/sendMessage?chat_id=5301913989&text=By, Bro!')
 
